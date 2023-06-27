2adc00a4a89bb5fb93ab096fb437a7e82db177e7
ridma <ridma@RIDMA_LAPTOP>
2023-06-27 20:32:33 +0530

    time 8.31 //pravindu code line 29-70 wenakan balanna awla.

--- LPDA/Login Form.cs
+++ LPDA/Login Form.cs
@@ -16,5 +16,70 @@ namespace LPDA
         {
             InitializeComponent();
         }
+
+        private void user_name_text_box_TextChanged(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void user_name_text_box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                
+                bool UserNameIsCorrect = false;
+
+                while (UserNameIsCorrect!= true)
+                {
+                    string user_name_login = user_name_text_box.Text;
+
+                    if (user_name_login != "ridma")
+                    {
+                       
+
+                        user_name_error_massege.Visible = true;
+                        user_name_error_picture.Visible = true;
+                        user_name_correct_picture.Visible = false;
+
+                        // Optionally, you can clear the username and password fields
+                        user_name_text_box.Clear();
+
+                        // Set focus back to the username field
+                        user_name_text_box.Focus();
+
+                        //---------------------------------
+
+                        UserNameIsCorrect = true;
+                        break;
+                    }
+                    else{
+                        user_name_error_massege.Visible = false;
+                        user_name_error_picture.Visible = false;
+
+                        // user name is correct 
+                        
+                        user_name_correct_picture.Visible = true;
+                        password_txet_box.Focus();
+                        UserNameIsCorrect = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void user_name_error_massege_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_txet_box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if()
+            }
+        }
     }
 }
